eda25ce86f9ebfcefc686bfba0656a9592368e8a
汤波 <478185912@qq.com>
2017-12-17 21:08:48 +0800

    init

---
+++ TCPServer/TCPServer/AsyncUserToken.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServer
+{
+    public class AsyncUserToken
+    {
+        /// <summary>  
+        /// 客户端IP地址  
+        /// </summary>  
+        public IPAddress IPAddress { get; set; }
+
+        /// <summary>  
+        /// 远程地址  
+        /// </summary>  
+        public EndPoint Remote { get; set; }
+
+        /// <summary>  
+        /// 通信SOKET  
+        /// </summary>  
+        public Socket Socket { get; set; }
+
+        /// <summary>  
+        /// 连接时间  
+        /// </summary>  
+        public DateTime ConnectTime { get; set; }
+
+        /// <summary>  
+        /// 所属用户信息  
+        /// </summary>  
+        //public UserInfoModel UserInfo { get; set; }
+
+
+        /// <summary>  
+        /// 数据缓存区  
+        /// </summary>  
+        public List<byte> Buffer { get; set; }
+
+
+        public AsyncUserToken()
+        {
+            this.Buffer = new List<byte>();
+        }
+    }
+}
