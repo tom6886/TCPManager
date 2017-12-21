b07a9cd02f6862af653b7713c4c26ba617340e13
汤波 <478185912@qq.com>
2017-12-21 13:32:55 +0800

    将TCP服务端主程序改为nuget引用
    nuget包：XTCPHandler

--- TCPServer/TCPServer/Program.cs
+++ TCPServer/TCPServer/Program.cs
@@ -1,43 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using TCPHandler;
 
 namespace TCPServer
 {
     class Program
     {
+        static SocketListener listener;
+
         static void Main(string[] args)
         {
             string com = "";
             do
             {
                 com = Console.ReadLine().ToUpper();
+
                 switch (com)
                 {
-                    case "TCP":
+                    case "NEWTCP":
                         try
                         {
-                            SocketConfig config = new SocketConfig()
-                            {
-                                MaxConnectCount = 200,
-                                MaxBufferSize = 1024,
-                                EndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 13909)
-                            };
+                            listener = new SocketListener(200, 1024);
 
-                            SocketManager m_socket = new SocketManager(config);
+                            listener.Init();
 
-                            m_socket.Init();
+                            listener.Start(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 13909));
 
-                            m_socket.Start();
+                            listener.OnClientNumberChange += Listener_OnClientNumberChange;
 
-                            m_socket.ReceiveClientData += M_socket_ReceiveClientData;
+                            listener.GetIDByEndPoint += Listener_GetIDByEndPoint;
 
-                            m_socket.ClientNumberChange += M_socket_ClientNumberChange;
+                            listener.GetPackageLength += Listener_GetPackageLength;
 
-                            Console.WriteLine("服务启动");
+                            listener.OnMsgReceived += Listener_OnMsgReceived;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(string.Format("发生错误：{0}", ex.Message));
+                        }
+                        break;
+                    case "UID":
+                        try
+                        {
+                            if (listener == null) { Console.WriteLine("请先初始化TCP服务"); break; }
+
+                            string[] uids = listener.OnlineUID;
+
+                            Console.WriteLine(String.Join(",", uids));
                         }
                         catch (Exception ex)
                         {
@@ -53,19 +63,33 @@ namespace TCPServer
             } while (com.ToUpper() != "EXIT");
         }
 
-        private static void M_socket_ClientNumberChange(int num, AsyncUserToken token)
+        private static void Listener_OnMsgReceived(TCPHandler.AsyncUserToken token, byte[] info)
         {
-            Console.WriteLine(num > 0 ? "有设备接入" : "有设备断开");
-            Console.WriteLine(" 来源IP：" + token.IPAddress.ToString());
+            Console.WriteLine("接收到数据：");
+            Console.WriteLine(" 来源IP：" + token.Remote.Address.ToString());
+            Console.WriteLine(" 连接时间：" + token.ConnectTime.ToString());
+            Console.WriteLine(" 内容：" + Encoding.UTF8.GetString(info));
+        }
+
+        private static void Listener_OnClientNumberChange(int number, TCPHandler.AsyncUserToken token)
+        {
+            Console.WriteLine(number > 0 ? "有设备接入" : "有设备断开");
+            Console.WriteLine(" 来源IP：" + token.Remote.Address.ToString());
             Console.WriteLine(" 连接时间：" + token.ConnectTime.ToString());
         }
 
-        private static void M_socket_ReceiveClientData(AsyncUserToken token, byte[] buff)
+        private static int Listener_GetPackageLength(byte[] data, out int headLength)
         {
-            Console.WriteLine("接收到数据：");
-            Console.WriteLine(" 来源IP：" + token.IPAddress.ToString());
-            Console.WriteLine(" 连接时间：" + token.ConnectTime.ToString());
-            Console.WriteLine(" 内容：" + Encoding.UTF8.GetString(buff));
+            headLength = 4;
+            byte[] lenBytes = new byte[4];
+            Array.Copy(data, lenBytes, 4);
+            int packageLen = BitConverter.ToInt32(lenBytes, 0);
+            return packageLen;
+        }
+
+        private static string Listener_GetIDByEndPoint(IPEndPoint endPoint)
+        {
+            return endPoint.GetHashCode().ToString();
         }
     }
 }
